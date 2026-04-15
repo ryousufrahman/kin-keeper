8a3459753cb8813839a0ad0993dd47faa23d6bf0
yousufrahman <ryousuf766@gmail.com>
2026-04-15 15:45:17 +0600

    made the count section dynamic

--- src/Components/Pages/Home/Home-componensts/Count.jsx
+++ src/Components/Pages/Home/Home-componensts/Count.jsx
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AllContext } from "../../../../GlobalContext/GlobalContext";
 
 const Count = () => {
+
+  const allstate = useContext(AllContext)
+
+     const {
+             AllFriendsArray ,
+            allInterection 
+          } =allstate
+
   return (
     <div className="grid mt-5 p-4 md:mt-10 gap-4 sm:grid-cols-2  md:grid-cols-4">
       <div className="bg-white rounded-xl shadow flex flex-col items-center justify-center py-8 ">
         <span className="text-[40px] font-bold text-[#284f42] leading-none mb-3">
-          10
+          {AllFriendsArray.length}
         </span>
         <span className="text-[#64748b] text-base font-medium">
           Total Friends
@@ -13,26 +22,27 @@ const Count = () => {
       </div>
       <div className="bg-white rounded-xl shadow flex flex-col items-center justify-center py-8 ">
         <span className="text-[40px] font-bold text-[#284f42] leading-none mb-3">
-          10
+          3
         </span>
         <span className="text-[#64748b] text-base font-medium">
-          Total Friends
+          On Track
         </span>
       </div>
       <div className="bg-white rounded-xl shadow flex flex-col items-center justify-center py-8 ">
         <span className="text-[40px] font-bold text-[#284f42] leading-none mb-3">
-          10
+          3
         </span>
         <span className="text-[#64748b] text-base font-medium">
-          Total Friends
+          Need Attention
+          
         </span>
       </div>
       <div className="bg-white rounded-xl shadow flex flex-col items-center justify-center py-8 ">
         <span className="text-[40px] font-bold text-[#284f42] leading-none mb-3">
-          10
+          {allInterection.length}
         </span>
         <span className="text-[#64748b] text-base font-medium">
-          Total Friends
+          Interactions This Month
         </span>
       </div>
     </div>
